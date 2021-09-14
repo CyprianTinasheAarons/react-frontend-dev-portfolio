b0c8759066170649e14cfb772839d51520641572
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2021-09-14 19:11:54 +0200

    Updates

---
+++ precache-manifest.eb318913aeeac2597e60906300015a6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d3e5428c7daf25a34a9c493565feb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cc6274b118f748531f1",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "b63bb443173c55388dd2",
+    "url": "/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "3cc6274b118f748531f1",
+    "url": "/static/js/2.b1be8f5e.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.b1be8f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63bb443173c55388dd2",
+    "url": "/static/js/main.c6243a6a.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.fbccc30162da19b2592bd8f7187cf98e.js"
+  "/precache-manifest.eb318913aeeac2597e60906300015a6c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
